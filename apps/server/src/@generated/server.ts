5d7575e9e3cd63d90c8819ea7f4d53539546e498
skaneprime <skaneprime@gmail.com>
2025-05-22 19:48:19 +0300

    fix: add generated to git pool

---
+++ apps/server/src/@generated/server.ts
@@ -0,0 +1,74 @@
+import { initTRPC } from "@trpc/server";
+import { z } from "zod";
+
+const t = initTRPC.create();
+const publicProcedure = t.procedure;
+
+const appRouter = t.router({
+  users: t.router({
+    getUserById: publicProcedure.input(z.object({ userId: z.string() })).output(z.object({
+      name: z.string(),
+      email: z.string(),
+      password: z.string(),
+    })).query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any)
+  }),
+  game: t.router({
+    createGame: publicProcedure.input(z.object({
+      userId: z.string(),
+      rows: z.number(),
+      cols: z.number(),
+      mines: z.number(),
+      betAmount: z.number(),
+      session: z.string(),
+    })).output(z.any()).mutation(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
+    revealCell: publicProcedure.input(z.object({
+      gameId: z.string(),
+      row: z.number(),
+      col: z.number(),
+    })).output(z.object({
+      gameId: z.string(),
+      grid: z.array(z.array(z.string())),
+      state: z.string(),
+    })).mutation(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
+    getLastUnfinishedGame: publicProcedure.input(z.object({ userId: z.string() })).output(z
+      .object({
+        userId: z.string(),
+        mines: z.number(),
+        id: z.string(),
+        roundId: z.string(),
+        grid: z.any(), // Using z.any() for JsonValue since it's a complex type
+        state: z.string(), // Using z.string() for $Enums.GameState
+        betTRXId: z.string(),
+        winTRXId: z.string(),
+        createdAt: z.date(),
+        updatedAt: z.date(),
+        betAmount: z.number(),
+      })
+      .nullable()).query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
+    cashOut: publicProcedure.input(z.object({
+      gameId: z.string(),
+      session: z.string(),
+      multiplier: z.number(),
+    })).output(z.object({
+      gameId: z.string(),
+      grid: z.array(z.array(z.string())),
+      state: z.string(),
+    })).mutation(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any),
+    loadLastGame: publicProcedure.input(z.object({ userId: z.string() })).output(z
+      .object({
+        userId: z.string(),
+        mines: z.number(),
+        id: z.string(),
+        roundId: z.string(),
+        grid: z.any(), // Using z.any() for JsonValue since it's a complex type
+        state: z.string(), // Using z.string() for $Enums.GameState
+        betTRXId: z.string(),
+        winTRXId: z.string(),
+        createdAt: z.date(),
+        updatedAt: z.date(),
+      })
+      .nullable()).query(async () => "PLACEHOLDER_DO_NOT_REMOVE" as any)
+  })
+});
+export type AppRouter = typeof appRouter;
+
